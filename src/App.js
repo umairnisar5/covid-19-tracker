fe6f1d8c43b58249fa280f2163ebb3386fdb92db
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-05 14:08:54 +0500

    covid 19

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React from 'react';
+// import React from 'react';
 
-import { Cards, CountryPicker, Chart } from './components';
-import { fetchData } from './api/';
-import styles from './App.module.css';
+// import { Cards, CountryPicker, Chart } from './components';
+// import { fetchData } from './api/';
+// import styles from './App.module.css';
 
-import image from './images/image.png';
+// import image from './images/image.png';
 
-class App extends React.Component {
-  state = {
-    data: {},
-    country: '',
-  }
+// class App extends React.Component {
+//   state = {
+//     data: {},
+//     country: '',
+//   }
 
-  async componentDidMount() {
-    const data = await fetchData();
+//   async componentDidMount() {
+//     const data = await fetchData();
 
-    this.setState({ data });
-  }
+//     this.setState({ data });
+//   }
 
-  handleCountryChange = async (country) => {
-    const data = await fetchData(country);
+//   handleCountryChange = async (country) => {
+//     const data = await fetchData(country);
 
-    this.setState({ data, country: country });
-  }
+//     this.setState({ data, country: country });
+//   }
 
-  render() {
-    const { data, country } = this.state;
+//   render() {
+//     const { data, country } = this.state;
 
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} /> 
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className={styles.container}>
+//         <img className={styles.image} src={image} alt="COVID-19" />
+//         <Cards data={data} />
+//         <CountryPicker handleCountryChange={this.handleCountryChange} />
+//         <Chart data={data} country={country} /> 
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { NativeSelect, FormControl } from '@material-ui/core';
+// import React, { useState, useEffect } from 'react';
+// import { NativeSelect, FormControl } from '@material-ui/core';
 
-import { fetchCountries } from '../../api';
+// import { fetchCountries } from '../../api';
 
-import styles from './CountryPicker.module.css';
+// import styles from './CountryPicker.module.css';
 
-const Countries = ({ handleCountryChange }) => {
-  const [countries, setCountries] = useState([]);
+// const Countries = ({ handleCountryChange }) => {
+//   const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    const fetchAPI = async () => {
-      setCountries(await fetchCountries());
-    };
+//   useEffect(() => {
+//     const fetchAPI = async () => {
+//       setCountries(await fetchCountries());
+//     };
 
-    fetchAPI();
-  }, []);
+//     fetchAPI();
+//   }, []);
 
-  return (
-    <FormControl className={styles.formControl}>
-      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-        <option value="">United States</option>
-        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
-      </NativeSelect>
-    </FormControl>
-  );
-};
+//   return (
+//     <FormControl className={styles.formControl}>
+//       <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+//         <option value="">United States</option>
+//         {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+//       </NativeSelect>
+//     </FormControl>
+//   );
+// };
 
-export default Countries;
+// export default Countries;
